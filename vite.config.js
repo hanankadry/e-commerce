29ad251478b8b04db943c009b08477764a37a660
Hanan <hanan.kadry@outlook.com>
2025-02-18 23:30:55 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ['/e-commerce/'],
+  base: '/e-commerce/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
